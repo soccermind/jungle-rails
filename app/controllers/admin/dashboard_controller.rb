0b94ed50038b3628533b719f73409c2ea410ad6c
Alberto Olvera Sotres <mty2yyz@gmail.com>
2021-01-07 09:59:08 -0500

    Fixed admin security issue

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class Admin::DashboardController < ApplicationController
 
-  # http_basic_authenticate_with name: ENV["HTTP_BASIC_AUTH_USER"], password: ENV["HTTP_BASIC_AUTH_PASSWORD"]
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_AUTH_USER"], password: ENV["HTTP_BASIC_AUTH_PASSWORD"]
 
   def show
     @product_count = Product.count
